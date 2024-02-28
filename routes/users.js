854006585614c53b0d1a73fc8a038358baf17feb
odalysruano <156559535+odalysruano@users.noreply.github.com>
2024-02-28 11:01:22 -0500

    Update users.js
    Moving comment to describe correct route.

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,11 @@ var router = express.Router();
 const ensureLoggedIn = require("../config/ensureLoggedIn");
 const usersCtrl = require("../controllers/users");
 
-// GET /users/:username - sellers shop page
 router.get('/', usersCtrl.index);
 router.get('/:userId', usersCtrl.getUsr);
 router.get('/:userId/items', usersCtrl.usrItmPg);
 router.post('/:userId/items/add', usersCtrl.addItm);
+// GET /users/:username - sellers shop page
 router.get("/:username", usersCtrl.shop);
 
 module.exports = router;
