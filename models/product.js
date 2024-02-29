a56449fa4ba607ba69ff543d8ef5468f346ed756
jademoslares <jade.moslares@gmail.com>
2024-02-29 10:18:06 -0500

    Products displays

--- models/product.js
+++ models/product.js
@@ -20,6 +20,14 @@ const productSchema = new Schema(
       type: Number,
       required: true,
     },
+    description: {
+      type: String,
+      required: false,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
     seller: {
       type: Schema.Types.ObjectId,
       ref: "User",
